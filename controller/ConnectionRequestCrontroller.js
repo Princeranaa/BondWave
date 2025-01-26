461fdbdb27c0e107d043c1e90b53ed3e3b00de2c
Prince Rana <ranaprince45may@gmail.com>
2025-01-27 01:44:12 +0530

    Update the Code of ConnectionRequest and Connection model

--- controller/ConnectionRequestCrontroller.js
+++ controller/ConnectionRequestCrontroller.js
@@ -1,20 +1,69 @@
 const ConnectionRequest = require("../model/Connection_model");
-exports.conncetionRequest = async (req,res) => {
-    try {
-        const fromUserId = req.user._id;
-        const toUserId = req.params.toUserId;
-        const status = req.params.status;
+const User = require("../model/User_model");
 
-        // created a new object for the data 
-        const connectionRequest = new ConnectionRequest({fromUserId,toUserId,status})
-        
-        // save the data into the database
-        const data = await connectionRequest.save()
-        res.status(200).json({
-            message: 'Connection request sent successfully',
-            data
-        })
-    } catch (error) {
-     res.status(400).json({message: error.message});
+exports.conncetionRequest = async (req, res) => {
+  try {
+    const fromUserId = req.user._id;  // Assuming req.user is already populated
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    // Validate the status type ===>
+    const allowedStatus = ["ignored", "interested"];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status type",
+      });
     }
-}
+
+    // Fetch both users' details (including firstName)
+    const fromUser = await User.findById(fromUserId);
+    const toUser = await User.findById(toUserId);
+
+    // Check if both users exist
+    if (!fromUser || !toUser) {
+      return res.status(404).json({
+        message: "User not Found",
+      });
+    }
+
+    // Check if the connection already exists in the database
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+
+    if (existingConnectionRequest) {
+      return res.status(400).json({
+        message: "Connection request already exists",
+      });
+    }
+
+    // Create a new connection request object
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    // Save the connection request into the database
+    const data = await connectionRequest.save();
+
+    // Modify the message based on the status
+    let statusMessage = "";
+    if (status === "interested") {
+      statusMessage = `${fromUser.firstName} is interested in ${toUser.firstName}`;
+    } else if (status === "ignored") {
+      statusMessage = `${fromUser.firstName} has ignored ${toUser.firstName}`;
+    }
+
+    // Return the response
+    return res.status(200).json({
+      message: statusMessage,
+      data,
+    });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
--- model/Connection_model.js
+++ model/Connection_model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionSchema = new mongoose.Schema({
+const connectionSchema = new mongoose.Schema(
+  {
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
@@ -22,5 +23,16 @@ const connectionSchema = new mongoose.Schema({
   },
   { timestamps: true }
 );
+
+connectionSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
 
 module.exports = mongoose.model("Connection", connectionSchema);
