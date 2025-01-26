fc7a77510b09792030df2a86aec4fca2afbaa632
Prince Rana <ranaprince45may@gmail.com>
2025-01-27 00:05:46 +0530

    created the Request router file for the routing

--- router/RequestRouter.js
+++ router/RequestRouter.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const requestRouter = express.Router();
 
+const {userAuth} = require("../middleware/auth");
+const { conncetionRequest } = require("../controller/ConnectionRequestCrontroller");
+
+requestRouter.post("/request/send/:status/:toUserId", userAuth, conncetionRequest )
 
 module.exports = requestRouter;
+
