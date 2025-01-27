95da171cb67ad7bc373ed47dc2a9645cd2177040
Prince Rana <ranaprince45may@gmail.com>
2025-01-28 00:23:47 +0530

    created the All the User Request Router

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,16 +1,12 @@
 const ConnectionRequest = require("../model/Connection_model");
 
+const USER_INFO = ["firstName","lastName","age","gender","photoUrl","about","skills"]
 exports.UserConnectionRequest = async (req, res) => {
   try {
     const loggedInUser = req.user;
 
-    const USER_INFO = ["firstName","lastName","age","gender","photoUrl","about","skills"]
 
-    const connectionRequest = await ConnectionRequest.find({
-      toUserId: loggedInUser._id,
-      status: "interested",
-      
-    }).populate("fromUserId", USER_INFO)
+    const connectionRequest = await ConnectionRequest.find({toUserId: loggedInUser._id,status: "interested"}).populate("fromUserId", USER_INFO)
 
     res.status(200).json({
       message: "Data Fetch Successfully",
@@ -20,3 +16,25 @@ exports.UserConnectionRequest = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+exports.requestAllTheUser = async (req,res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequest.find({
+             $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" }
+              ]
+        }).populate("fromUserId", USER_INFO)
+        const data = connectionRequest.map(row=>row.fromUserId)
+
+
+        res.status(200).json({
+            message: "Connetion fetch successfully",
+            data
+        });
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
--- router/UserRouter.js
+++ router/UserRouter.js
@@ -2,10 +2,11 @@ const express = require('express')
 const userRouter = express.Router()
 
 const {userAuth} = require("../middleware/auth");
-const { UserConnectionRequest } = require('../controller/UserController');
+const { UserConnectionRequest, requestAllTheUser } = require('../controller/UserController');
 
 
 userRouter.get("/user/requests/received", userAuth, UserConnectionRequest)
+userRouter.get("/user/connection", userAuth, requestAllTheUser)
 
 
 
