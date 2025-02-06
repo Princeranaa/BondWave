a69f0adc5401389006b9944f8e02b2b31991ed01
Prince Rana <ranaprince45may@gmail.com>
2025-02-07 00:24:18 +0530

    install the Tailwind and daisyUI , and the Added the Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,13 @@ function App() {
    <>
    <div className="navbar bg-base-300">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">👦🏻 devTinder</a>
   </div>
   <div className="flex-none gap-2">
-    <div className="form-control">
+    {/* <div className="form-control">
       <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
+    </div> */}
+    <div className="dropdown dropdown-end mx-2">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
