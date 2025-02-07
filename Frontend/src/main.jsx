ee8b866b2cdc4cc2dae123d7dae651c348ea9c7d
Prince Rana <ranaprince45may@gmail.com>
2025-02-08 00:13:06 +0530

    `feat: updated main.jsx to include react-redux and app store setup`, "user Data Setup"

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore.js";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename='/'>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Provider store={appStore}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
