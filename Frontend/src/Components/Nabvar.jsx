0d8780554c04fee5d18430c8ee86a76c9627b673
Prince Rana <ranaprince45may@gmail.com>
2025-11-09 19:30:58 +0530

    modify the code

--- Frontend/src/Components/Nabvar.jsx
+++ Frontend/src/Components/Nabvar.jsx
@@ -1,50 +1,70 @@
+import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
-// import {useNavigate} from "react-redux"
-
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/Constant";
+import { useNavigate } from "react-router-dom";
+import { removeUser } from "../utils/userSlice";
 
 
 function Nabvar() {
   const user = useSelector((state) => state.user);
-  // const navigate = useNavigate()
-  console.log(user);
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        BASE_URL + "/logout",
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUser());
+      return navigate("login");
+    } catch (error) {
+      console.log("something want wrong", error);
+    }
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">👦🏻 devTinder</a>
       </div>
-     {user && ( <div className="flex-none gap-2">
-      <div>Welcome, {user.firstName} {user.lastName}</div>
-        <div className="dropdown dropdown-end gap-2 mx-2">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img alt="user photo" src={user.photoUrl} />
-            </div>
+      {user && (
+        <div className="flex-none gap-2">
+          <div>
+            Welcome, {user.firstName} {user.lastName}
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+          <div className="dropdown dropdown-end gap-2 mx-2">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="user photo" src={user.photoUrl} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>)}
+      )}
     </div>
   );
 }
