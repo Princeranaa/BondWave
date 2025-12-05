4a323f5ac043b9f32c77deaa70e42f00c3389d6a
Prince Rana <ranaprince45may@gmail.com>
2025-12-06 00:19:03 +0530

    update the UI

--- Frontend/src/Components/Premium.jsx
+++ Frontend/src/Components/Premium.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import { BASE_URL } from "../utils/Constant";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Premium() {
   const [isUserPremium, setIsUserPremium] = useState(false);
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore.js";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <Provider store={appStore}>
       <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+
 );
