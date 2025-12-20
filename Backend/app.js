ba1e7335a49e4f07ae966a408af320acceb4aa30
Prince Rana <ranaprince45may@gmail.com>
2025-12-20 18:43:49 +0530

    Revert specific unwanted commits

--- Backend/app.js
+++ Backend/app.js
@@ -1,39 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const cookie = require("cookie-parser");
-const allowedOrigins = [
-  "http://localhost:5173", // for local frontend
-  "https://bond-wave.vercel.app", // for production frontend
-];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use(cookie());
+const cors = require('cors');
+const cookie = require('cookie-parser');
+app.use(cookie())
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
-const Db = require("./config/database");
-Db.connectToDatabase();
+
+const Db = require("./config/database")
+Db.connectToDatabase()
 
 const AuthUser = require("./router/AuthUser");
 const profilerouter = require("./router/ProfileRouter");
 const requestRouter = require("./router/RequestRouter");
-const userRouter = require("./router/UserRouter");
-const paymentRouter = require("./router/payment");
-const chatRouter = require("./router/ChatRouter");
+const userRouter = require('./router/UserRouter');
+const paymentRouter = require('./router/payment')
+const chatRouter = require('./router/ChatRouter');
 
-app.use("/", AuthUser);
-app.use("/", profilerouter);
-app.use("/", requestRouter);
-app.use("/", userRouter);
-app.use("/", paymentRouter);
-app.use("/", chatRouter);
+app.use("/", AuthUser)
+app.use("/", profilerouter)
+app.use("/", requestRouter)
+app.use("/", userRouter)
+app.use("/", paymentRouter)
+app.use("/", chatRouter)
 
-module.exports = app;
+
+
+module.exports = app
