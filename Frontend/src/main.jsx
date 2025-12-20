2b3c3db72745c0475d0cf5e8c201e4b9c512a4d7
Prince Rana <ranaprince45may@gmail.com>
2025-12-20 17:37:07 +0530

    changes the BASE_URL

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore.js";
+import { BASE_URL } from "./utils/Constant.js";
+console.log("VITE_BASE_URL:", import.meta.env.VITE_BASE_URL);
+
+console.log("localcodeURL", BASE_URL)
+
 createRoot(document.getElementById("root")).render(
   
     <Provider store={appStore}>
--- Frontend/src/utils/Constant.js
+++ Frontend/src/utils/Constant.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = "http://localhost:3000"
 // export const BASE_URL = "https://bondwave.onrender.com"
-export const BASE_URL = "import.meta.env.VITE_BASE_URL;"
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || "http://localhost:3000";
+
