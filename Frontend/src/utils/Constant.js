76c2e651cdbbb8853ff38536950e04b8b9249f5f
Prince Rana <ranaprince45may@gmail.com>
2025-12-23 16:25:28 +0530

    setupt the enviroment file and origin

--- Frontend/src/utils/Constant.js
+++ Frontend/src/utils/Constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000"
-// export const BASE_URL = "https://bondwave.onrender.com"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
